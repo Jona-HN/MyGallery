8c85e2d90deefdd434b2883a6173b50708362a08
Jonathan Herrera <jherrera@certuit.com>
2022-03-28 19:56:18 -0700

    Se creó la clase MyPhoto que contiene dos atributos: la ruta de la foto, así como una bandera que indica si debe ser incluída en la galería

---
+++ src/main/java/com/jona/gallery/MyPhoto.java
@@ -0,0 +1,31 @@
+package com.jona.gallery;
+
+import java.io.Serializable;
+
+public class MyPhoto implements Serializable
+{
+    private String name;
+    private boolean selected;
+
+    public MyPhoto(String name, boolean selected)
+    {
+        this.name = name;
+        this.selected = selected;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+}
